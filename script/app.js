eab711947287bcb43b70123f3edc310f7ea6ae18
riqkml <riqkikamal@Outlook.com>
2020-06-28 21:48:17 +0700

    add liquid image

---
+++ script/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.9,
+  image1: "../image/calm.jpeg",
+  image2: "../image/peckle.jpeg",
+  displacementImage: "../image/fluid.jpg",
+  speedIn: 1,
+  speedOut: 2,
+});
